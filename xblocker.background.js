42520f9de7b931f8f2571200cef124e0308b96c6
Rudie Dirkx <github@hotblocks.nl>
2015-11-28 16:12:42 +0100

    #5 disable/ignore per tab

--- xblocker.background.js
+++ xblocker.background.js
@@ -1,4 +1,14 @@
 
+// Ignore blocking per tab
+var ignoredTabs = {};
+function setPageActionIcon(tabId, ignored) {
+	var icon = ignored ? '128x128-grayscale' : '128x128';
+	chrome.pageAction.setIcon({
+		tabId: tabId,
+		path: chrome.runtime.getURL('images/' + icon + '.png'),
+	});
+}
+
 // Init patterns from local storage
 console.time('Back-end init');
 var regexes;
@@ -24,14 +34,24 @@ var filter = {
 var extraInfoSpec = ['blocking'];
 chrome.webRequest.onBeforeRequest.addListener(function(details) {
 
+	// Ignore this tab
+	if (ignoredTabs[details.tabId]) {
+		setPageActionIcon(details.tabId, true);
+		chrome.pageAction.show(details.tabId);
+		return;
+	}
+
 	if ( !regexes ) return;
 
 	for (var i=0, L=regexes.length; i<L; i++) {
 		var regex = regexes[i];
 		if (regex.test(details.url)) {
+
 			console.log('BLOCKING', details.url);
 
-			chrome.pageAction.show(details.tabId);
+			if (details.tabId) {
+				chrome.pageAction.show(details.tabId);
+			}
 
 			return {
 				cancel: true
@@ -51,5 +71,21 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 		console.log('Incoming RELOAD event handled from options page');
 
 		sendResponse();
+	}
+});
+
+
+
+// Listen for page action click
+chrome.pageAction.onClicked.addListener(function(tab) {
+	ignoredTabs[tab.id] = !ignoredTabs[tab.id];
+	setPageActionIcon(tab.id, ignoredTabs[tab.id]);
+
+	if (ignoredTabs[tab.id]) {
+		chrome.tabs.reload(tab.id);
+		console.log('IGNORING TAB:', tab.id)
+	}
+	else {
+		console.log('STOP IGNORING TAB:', tab.id)
 	}
 });
